6cbae363adfd28a405bffc9003fc4f70df8eb54d
Dishant Vaidya <dishantvaidya893@gmail.com>
2025-09-01 21:54:21 +0530

    solved new question
    learned new method (list comprehension)

---
+++ hackerrank/HR_Q7.py
@@ -0,0 +1,19 @@
+'''You are given three integers x, y and z representing the dimensions of a cuboid along with an integer n. 
+Print a list of all possible coordinates given by (i, j, k) on a 3D grid 
+where the sum of i + j + k is not equal to n. Here, 0 ≤ i ≤ x; 0 ≤ j ≤ y; 0 ≤ k ≤ z.
+'''
+x = int(input("Value of x :"))
+y = int(input("Value of y :"))
+z = int(input("Value of z :"))
+n = int(input("Value of n :"))
+
+#List comprehension
+permutation = [
+    [i, j, k]
+    for i in range(x + 1)
+    for j in range(y + 1)
+    for k in range(z + 1)
+    if i + j + k != n
+]
+
+print(permutation)
